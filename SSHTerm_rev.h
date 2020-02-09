2a353328e3a27b477fe208edd095dd2ddc67569e
Fredrik Wikstrom <fredrik@a500.org>
2020-02-09 11:26:57 +0200

    Bumped version to 1.2.

--- SSHTerm_rev.h
+++ SSHTerm_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	1
-#define DATE		"29.1.2020"
-#define VERS		"SSHTerm 1.1"
-#define VSTRING		"SSHTerm 1.1 (29.1.2020)\r\n"
-#define VERSTAG		"\0$VER: SSHTerm 1.1 (29.1.2020)"
+#define REVISION	2
+#define DATE		"9.2.2020"
+#define VERS		"SSHTerm 1.2"
+#define VSTRING		"SSHTerm 1.2 (9.2.2020)\r\n"
+#define VERSTAG		"\0$VER: SSHTerm 1.2 (9.2.2020)"
