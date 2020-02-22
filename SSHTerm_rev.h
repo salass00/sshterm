860f4936f043ca4446754ef2c43a5b90f9ca9361
Fredrik Wikstrom <fredrik@a500.org>
2020-02-22 10:31:57 +0200

    Bumped version to 1.3.

--- SSHTerm_rev.h
+++ SSHTerm_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	2
-#define DATE		"9.2.2020"
-#define VERS		"SSHTerm 1.2"
-#define VSTRING		"SSHTerm 1.2 (9.2.2020)\r\n"
-#define VERSTAG		"\0$VER: SSHTerm 1.2 (9.2.2020)"
+#define REVISION	3
+#define DATE		"22.2.2020"
+#define VERS		"SSHTerm 1.3"
+#define VSTRING		"SSHTerm 1.3 (22.2.2020)\r\n"
+#define VERSTAG		"\0$VER: SSHTerm 1.3 (22.2.2020)"
