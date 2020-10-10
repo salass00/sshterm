4086a6254357d9ccad01b259dc376882aaec991e
Fredrik Wikstrom <fredrik@a500.org>
2020-10-10 09:17:18 +0300

    Fixed a compiler warning when compiling with older roadshow includes.

--- src/bsdsocket-stubs.c
+++ src/bsdsocket-stubs.c
@@ -19,6 +19,9 @@
  * Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/* Fixes a warning when compiling with older roadshow includes */
+#define __timeval timeval
+
 #include <proto/bsdsocket.h>
 #include <sys/filio.h>
 #include <sys/socket.h>
