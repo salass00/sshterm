72ce955137dbd4c6c2de520802736efced2e5032
Fredrik Wikstrom <fredrik@a500.org>
2020-10-30 13:38:06 +0200

    Bumped version to 1.8.

--- SSHTerm_rev.h
+++ SSHTerm_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	7
-#define DATE		"23.3.2020"
-#define VERS		"SSHTerm 1.7"
-#define VSTRING		"SSHTerm 1.7 (23.3.2020)\r\n"
-#define VERSTAG		"\0$VER: SSHTerm 1.7 (23.3.2020)"
+#define REVISION	8
+#define DATE		"30.10.2020"
+#define VERS		"SSHTerm 1.8"
+#define VSTRING		"SSHTerm 1.8 (30.10.2020)\r\n"
+#define VERSTAG		"\0$VER: SSHTerm 1.8 (30.10.2020)"
